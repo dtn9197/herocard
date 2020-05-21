8c22929a1e753fb0c36d2a4f8ba2a3b1e4331bb3
Duy Nguyen <dtn9197@gmail.com>
2020-05-21 19:33:34 -0400

    commit

--- src/HeroCard.js
+++ src/HeroCard.js
@@ -14,26 +14,68 @@
 
 import {LitElement, html, css} from 'lit-element';
 
+import { SimpleColors } from '@lrnwebcomponents/simple-colors/simple-colors';
 
-export class HeroCard extends LitElement {
+
+export class HeroCard extends SimpleColors {
 
   static get styles() {
     return css`
         .hero-card {
         position: relative;
-        border: 15px solid rgb(170, 121, 57);
+        border: 5px solid rgb(8, 37, 218 );
         width: 400px;
         height: 100%;
-        background-color: grey;
-       
+        /* background-color: black; */
+        /* background-image: url("../demo/assets/background2.jpg"); */
+        background-color: black;
+        background-size: cover;
+        background-position: center;
+        color: white;
+        
       }
 
-      img {
+      .hero-card * {
+        margin: 0px;
+      }
+
+      #footer {
+        background-image: url("../demo/assets/background2.jpg");
+        background-size: cover;
+        background-position: center;
+        height: 25px;
+      }
+
+      #header {
+        background-image: url("../demo/assets/background2.jpg");
+        background-size: cover;
+        background-position: center;
+        height: 75px;
+      }
+
+      
+      #heroimage {
+        border-top: 3px solid rgb(31, 142, 175);
+        border-bottom: 3px solid rgb(31, 142, 175);
         width: 100%;
+      }
+      #heroname {
+        text-transform: uppercase;
+        font-family: Arial, Helvetica, sans-serif;
+        font-style: italic;
+        color: rgb(241, 201, 20)
+      }
+
+      #type {
+        margin: 0px;
+        color: rgb(108, 187, 249 );
+      }
+
+      hr {
+        border-bottom: 3px solid rgb(31, 142, 175);
       }
 
       #logo {
-        display: inline;
         width: 50px;
         position: absolute;
         right: 0px;
@@ -55,7 +97,8 @@ export class HeroCard extends LitElement {
       hero: {type: String},
       powers: {type: String},
       heroimage: {type: String},
-      universe: {type: String}
+      universe: {type: String},
+      type: {type: String}
 
     };
   }
@@ -67,6 +110,7 @@ export class HeroCard extends LitElement {
     this.powers = "";
     this.heroimage = "";
     this.universe = "";
+    this.type = "";
   }
 
   render() {
@@ -80,25 +124,35 @@ export class HeroCard extends LitElement {
       
     
     return html`
-     <div class="hero-card">
-      <h1>${this.hero}<h1><img id="logo" src=${logoUsed}>
-        <hr>
-        <img id ="heroimage" src=${this.heroimage} alt="">
-        <h2>Description</h2>
-        <hr>
-        <slot name="description">
+    
+        <div class="hero-card">
+          <section id="header">
+              <h1 id="heroname">${this.hero}</h1>
+              <img id="logo" src=${logoUsed}>
+              <h2 id="type">${this.type}</h2>
+          </section>
+            <img id ="heroimage" src=${this.heroimage} alt="">
 
-        </slot>
-        <h2>Powers</h2>
-        <hr>
-        <ul>
-          <div class="grid-container">
-            ${powers.map(power => 
-              html`<li class="grid-item"> ${power} </li>`
-              )}
-          </div>
-        </ul>
-    </div>
+            <section id="content">
+                <h2>Description</h2>
+                <hr>
+                <slot name="description">
+                </slot>
+            
+                <h2>Powers</h2>
+                <hr>
+                <ul>
+                  <div class="grid-container">
+                    ${powers.map(power => 
+                      html`<li class="grid-item"> ${power} </li>`
+                      )}
+                  </div>
+                </ul>
+            </section>
+
+            <section id="footer"></section>
+            
+        </div>
     `;
   }
 
